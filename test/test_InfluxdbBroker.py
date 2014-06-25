de4881e6dd0f603cb00fcd8473ed23b8f42f4467
aviau <alexandre.viau@savoirfairelinux.com>
2014-06-25 13:27:42 -0400

    Added test for get_check_result_perfdata_points

--- test/test_InfluxdbBroker.py
+++ test/test_InfluxdbBroker.py
@@ -1,7 +1,26 @@
 from module.module import InfluxdbBroker
+import unittest
 
 
-class TestInfluxdbBroker:
+class TestInfluxdbBroker(unittest.TestCase):
 
-    def test_example(self):
-        assert 'c' == 'c'
+    def test_get_check_result_perfdata_points(self):
+        name = 'testname'
+        data = {
+            'perf_data': 'ramused=1009MB;;;0;1982 swapused=540PT;;;0;3827 memused=1550GB;2973;3964;0;5810',
+            'last_chk': 1403618279,
+        }
+
+        expected = [
+            {'points': [[1403618279, 1009, 'MB', None, None, 0, 1982]],
+             'name': 'testname.ramused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']},
+            {'points': [[1403618279, 1550, 'GB', 2973, 3964, 0, 5810]],
+             'name': 'testname.memused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']},
+            {'points': [[1403618279, 540, 'PT', None, None, 0, 3827]],
+             'name': 'testname.swapused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']}
+        ]
+        result = InfluxdbBroker.get_check_result_perfdata_points(data, name)
+        self.assertEqual(expected, result)
