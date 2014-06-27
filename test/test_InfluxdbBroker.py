8923f88d38766943b37fbf62b9b3805222045ea1
aviau <alexandre.viau@savoirfairelinux.com>
2014-06-27 10:39:48 -0400

    Added test_get_unknown_check_result_perfdata_points

--- test/test_InfluxdbBroker.py
+++ test/test_InfluxdbBroker.py
@@ -14,6 +14,28 @@ class TestInfluxdbBroker(unittest.TestCase):
             }
         )
 
+    def test_get_unknown_check_result_perfdata_points(self):
+        name = 'testname'
+        data = {
+            'perf_data': 'ramused=1009MB;;;0;1982 swapused=540PT;;;0;3827 memused=1550GB;2973;3964;0;5810',
+            'time_stamp': 1403618279,
+        }
+
+        expected = [
+            {'points': [[1403618279, 1009, 'MB', None, None, 0, 1982]],
+             'name': 'testname.ramused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']},
+            {'points': [[1403618279, 1550, 'GB', 2973, 3964, 0, 5810]],
+             'name': 'testname.memused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']},
+            {'points': [[1403618279, 540, 'PT', None, None, 0, 3827]],
+             'name': 'testname.swapused',
+             'columns': ['time', 'value', 'unit', 'warning', 'critical', 'min', 'max']}
+        ]
+
+        result = InfluxdbBroker.get_unknown_check_result_perfdata_points(data, name)
+        self.assertEqual(expected, result)
+
     def test_get_check_result_perfdata_points(self):
         name = 'testname'
         data = {
