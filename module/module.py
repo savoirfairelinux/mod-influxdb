b7f462e2bce4aebcdf977f01b54ce9442f79dfb7
aviau <alexandre.viau@savoirfairelinux.com>
2014-11-12 11:24:39 -0500

    Include logEvent in the module

--- module/module.py
+++ module/module.py
@@ -37,6 +37,12 @@ from shinken.misc.perfdata import PerfDatas
 from shinken.misc.logevent import LogEvent
 from influxdb import InfluxDBClient
 
+# LogEvent is only available in shinken>2.0.3
+try:
+    from shinken.misc.logevent import LogEvent
+except ImportError:
+    from logevent import LogEvent
+
 properties = {
     'daemons': ['broker'],
     'type': 'influxdb_perfdata',
